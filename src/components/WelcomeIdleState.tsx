0b09d8f7fa7f4ff22238d1595da83abbcd38496c
Swarup R. Paul <121659337+SwarupCreates@users.noreply.github.com>
2026-08-14 04:05:30 +0530

    Fix welcome padding observer issue by using a spacer div

--- src/components/WelcomeIdleState.tsx
+++ src/components/WelcomeIdleState.tsx
@@ -173,7 +173,8 @@ export function WelcomeIdleState({ weather }: WelcomeIdleStateProps) {
   }, [setBottomUIHeight]);
 
   return (
-    <div ref={containerRef} className="pointer-events-none absolute bottom-[140px] left-0 right-0 z-20 flex flex-col px-5 pt-6">
+    <div ref={containerRef} className="pointer-events-none absolute bottom-[140px] left-0 right-0 z-20 flex flex-col px-5">
+      <div className="h-6 shrink-0 w-full" />
       <AnimatePresence>
         {error && (
           <motion.div 
